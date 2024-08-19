01c1366105bef7cbe31d3c6ae6fa4844e56e464c
Abdelghani Moussaid <20841568+abdelghani-moussaid@users.noreply.github.com>
2024-08-19 16:51:45 +0100

    add Tree class

---
+++ build-tree.js
@@ -0,0 +1,22 @@
+import Node from "./node.js";
+
+function buildTree(array)
+{
+    /* Base Case */
+    if (start > end)
+    {
+        return null;
+    }
+    /* Get the middle element and make it root */
+    let mid = Math.floor((array.length - 1) / 2);
+    let node = new Node(arr[mid]);
+    /* Recursively construct the left subtree and make it 
+     left child of root */
+    node.left = buildTree(array.slice(0, mid));
+    /* Recursively construct the right subtree and make it 
+     right child of root */
+    node.right = buildTree(array.slice(mid + 1));
+    return node;
+}
+
+
